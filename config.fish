426a69e806a9497439b4ea285e96132f10570f8d
Thakurdeep Singh <thakurdeep1789@gmail.com>
2026-09-05 21:08:22 -0400

    added icons to tree

--- config.fish
+++ config.fish
@@ -11,7 +11,7 @@ if status is-interactive
     alias fd fdfind
     alias ls 'eza -g --color=auto --group-directories-first'
     alias ll 'eza -lg --git --icons --group-directories-first'
-    alias lt 'eza -g --tree --icons'
+    alias lt 'eza -g --tree --icons always'
     alias l 'eza -1 --icons always'
     alias vi nvim
     alias clip='xclip -selection c'
